444ab9137f0cede9e6fca6d6da3c9b3053b9a4a1
Green <shren0812@gmail.com>
2022-06-24 15:15:16 +0900

    캔버스 바탕화면 색깔 변경

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -47,7 +47,7 @@ export default class App {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     // fill canvas background
-    this.context.fillStyle = "black";
+    this.context.fillStyle = "#333333";
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
     const gravity = new CreateVector(0, 2);
